d752db6f30252590b1b1a1f464e0b2aa0ff0d3f4
nehpets0701 <nehpets0701@gmail.com>
2020-09-16 17:32:14 +0000

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,12 @@
 #include<stdio.h>
+
+/**
+ *main-print line
+ *Return:0
+ */
 int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art,");
+printf("with proper grammar, but the outcome is a piece of art,");
 
-  return (0);
+return (0);
 }
