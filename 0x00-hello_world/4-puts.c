17d6bd9dbe5a59e34d43f63f0c62aa3f6b449581
nehpets0701 <nehpets0701@gmail.com>
2020-09-16 17:26:22 +0000

    printline

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include<stdio.h>
+
+/**
+ *main-printing line
+ *Return:0
+ */
 int main(void)
 {
-  puts(\"Programming is like building a multilingual puzzle);
+puts(\"Programming is like building a multilingual puzzle);
 
-  return 0;
+
+return 0;
 }
