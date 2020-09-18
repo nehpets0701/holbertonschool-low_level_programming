6aa01fd245a0a251ac07044d693161a91a282468
nehpets0701 <nehpets0701@gmail.com>
2020-09-18 17:46:57 +0000

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ *main-comb3
+ *Return:0
+ */
+int main(void)
+{
+	int num1;
+	int num2;
+
+	for (num1 = 48 ; num1 <= 57 ; num1++)
+	{
+		/*putchar(num1);*/
+
+		for (num2 = 49 ; num2 <= 57 ; num2++)
+		{
+			if(num1 != num2)
+			{
+			putchar(num1);
+			putchar(num2);
+			putchar(',');
+			putchar(' ');
+			}
+		}
+	}
+
+	return (0);
+}
