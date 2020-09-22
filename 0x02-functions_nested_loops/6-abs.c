8642e0a7a0ccf7c4fde251f49321b74d17cd5fde
nehpets0701 <nehpets0701@gmail.com>
2020-09-22 17:29:26 +0000

    if

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,6 +1,18 @@
 #include "holberton.h"
 
-int _abs(int)
+/**
+ *_abs-absolutevalue
+ *Return: number
+ *@int:input
+ */
+int _abs(int i)
 {
-	
+	if (i >= 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (i * -1);
+	}
 }
