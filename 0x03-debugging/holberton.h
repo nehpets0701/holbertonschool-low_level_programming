122e14f72d426faa9d32881f9364946a1c972377
nehpets0701 <nehpets0701>
2020-09-23 18:23:56 +0000

    header

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -28,5 +28,7 @@ void times_table(void);
 
 int largest_number(int a, int b, int c);
 
-void print_remaining(int month, int day, int year);
+void print_remaining_days(int month, int day, int year);
+
+int convert_day(int month, int day);
 #endif /*holberton_h*/
