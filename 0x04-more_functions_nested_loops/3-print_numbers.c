c43818b2fad23a59af85aed0415067154845bf2b
nehpets0701 <nehpets0701@gmail.com>
2020-09-24 13:54:14 +0000

    print

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ *print_numbers-print
+ *Return: 0
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48 ; i <= 57 ; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
