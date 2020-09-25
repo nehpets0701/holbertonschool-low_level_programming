5eec870081f977e52efced8cf3462f30f011eacc
nehpets0701 <nehpets0701@gmail.com>
2020-09-25 17:12:03 +0000

    14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,12 +7,16 @@
 void more_numbers(void)
 {
 	int i;
+	int n;
 
-	for (i = 0 ; i <= 14 ; i++)
+	for (n = 0 ; n < 10 ; n++)
 	{
-		if (i > 9)
-			_putchar(i / 10 + '0');
-		_putchar(i % 10 + '0');
+		for (i = 0 ; i <= 14 ; i++)
+		{
+			if (i > 9)
+				_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+		}
 	}
 	_putchar('\n');
 }
