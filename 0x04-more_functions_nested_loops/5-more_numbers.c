dc74e02373f3f0be585f1aed706c33bd0da2b266
nehpets0701 <nehpets0701@gmail.com>
2020-09-25 17:10:27 +0000

    14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,18 +1,18 @@
 #include "holberton.h"
 
 /**
- *print_most_numbers-no24
+ *more_numbers-no24
  *Return: 0
  */
-void print_most_numbers(void)
+void more_numbers(void)
 {
 	int i;
 
 	for (i = 0 ; i <= 14 ; i++)
 	{
-		_putchar(i % 10 + '0');
 		if (i > 9)
 			_putchar(i / 10 + '0');
+		_putchar(i % 10 + '0');
 	}
 	_putchar('\n');
 }
