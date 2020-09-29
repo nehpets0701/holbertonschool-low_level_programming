f0dabd87781e1d743fcff2e0c24b3b83a115d845
nehpets0701 <nehpets0701@gmail.com>
2020-09-29 19:25:39 +0000

    array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ *print_array-array
+ *Return:0
+ *@a:input
+ *@n:input
+ */
+void print_array(int *a, int n)
+{
+	int index = 0;
+	
+	while (index <= n)
+	{
+		if (index != n)
+		{
+			printf("%d, ", a[index]);
+			index++;
+		}
+		else
+		{
+			printf("%d", a[index]);
+			index++;
+		}
+	}
+
+	printf("\n");
+}
