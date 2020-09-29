3ca9446d2b312e41380c43d27634c40b2148c449
nehpets0701 <nehpets0701@gmail.com>
2020-09-29 18:14:16 +0000

    half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ *puts_half-half
+ *Return:0
+ *@str:input
+ */
+void puts_half(char *str)
+{
+	int length = 0;
+	int a;
+
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+
+	if (length % 2 != 0)
+	{
+		length--;
+	}
+
+	for (a = 0 ; a != length ; a++)
+	{
+		if (a >= length / 2)
+		{
+			_putchar(str[a]);
+		}
+	}
+
+	_putchar('\n');
+}
