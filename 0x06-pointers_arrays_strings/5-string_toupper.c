3720edbcb9afdfc9e7e0ceb446b9e7cdf66b7013
nehpets0701 <nehpets0701@gmail.com>
2020-10-01 19:33:29 +0000

    upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ *string_toupper-toupper
+ *Return:char
+ *@s:input
+ */
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0 ; *s != '\0' ; i++)
+	{
+		if (s[i] >= 97)
+		{
+			s[i] = s[i] - 32;
+			s++;
+		}
+	}
+	return (s);
+}
