6342d27e07c239b55f25f56b05f5481c2b9162b0
nehpets0701 <nehpets0701@gmail.com>
2020-10-02 18:01:12 +0000

    cap

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ *cap_string-capital
+ *Return:char
+ *betty comment????????
+ */
+char *cap_string(char *)
+{
+	char[] letters = {' ', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+	int i;
+
+	for (i = 0 ; i < 
+}
