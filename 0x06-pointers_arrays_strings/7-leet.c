50ea4acb1637cada79f52abd0a5ec0dee9249d5e
nehpets0701 <nehpets0701@gmail.com>
2020-10-02 18:03:31 +0000

    leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ *leet-replace
+ *Return:char
+ *@s:input
+ */
+char *leet(char *s)
+{
+	letters[10] = {a, A, e, E, o, O, t, T, l, L};
+
+	while (*s != '\0')
+	{
+		
+	}
+}
