4a21f7fb5f9f8af7e877bd73b4dc8c1d3a0ea27f
nehpets <nehpets0701@gmail.com>
2020-10-04 21:18:25 +0000

    rot13

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -5,7 +5,22 @@
  *Return:char
  *betty comment???????
  */
-char *rot13(char *)
+char *rot13(char *s)
 {
+	int i;
+	int j;
 
+	char alpha[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for (i = 0 ; s[i] != 0 ; i++)
+	{
+		for (j = 0 ; alpha[j] != 0 ; j++)
+		{
+			if (s[i] == alpha[j])
+			{
+				s[i] = rot13[j];
+			}
+		}
+	}
 }
