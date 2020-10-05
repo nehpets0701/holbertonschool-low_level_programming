c2948c1f23bfe2ed8f3b34e0c45f010bf0004352
nehpets0701 <nehpets0701@gmail.com>
2020-10-05 17:38:14 +0000

    strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,12 +9,11 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	int i;
-        int j;
+	int j;
 
-
-	for (i = 0; s[i]; i++) 
+	for (i = 0; s[i]; i++)
 	{
-		for (j = 0; accept[j]; j++) 
+		for (j = 0; accept[j]; j++)
 		{
 			if (accept[j] == s[i])
 				break;
