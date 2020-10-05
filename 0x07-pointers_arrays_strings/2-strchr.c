b181f807cc7088f17a4214624deb922bc6054c48
nehpets0701 <nehpets0701@gmail.com>
2020-10-05 16:51:39 +0000

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,14 +10,14 @@ char *_strchr(char *s, char c)
 {
 	int i = 0;
 
-	while (s[i] != c && s[i] != '\0')
+	while (s[i] != '\0')
 	{
-		i++;
-
 		if (s[i] == c)
 		{
-			return (s);
+			return (s + i);
 		}
+
+		i++;
 	}
 
 	return (0);
