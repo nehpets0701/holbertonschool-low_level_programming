3418f7823b920f5ca773e281e32d703ea3ac5243
nehpets0701 <nehpets0701@gmail.com>
2020-10-05 17:36:35 +0000

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ *_strspn-length of substring
+ *Return:int bytes
+ *@s:string
+ *@accept:substring
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i;
+        int j;
+
+
+	for (i = 0; s[i]; i++) 
+	{
+		for (j = 0; accept[j]; j++) 
+		{
+			if (accept[j] == s[i])
+				break;
+		}
+		if (!accept[j])
+			break;
+	}
+	return (i);
+}
