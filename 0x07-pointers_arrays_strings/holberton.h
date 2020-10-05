9daf85d958a1d0c0841aca0dbb45f22c98944810
nehpets0701 <nehpets0701@gmail.com>
2020-10-05 21:00:33 +0000

    header

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -7,7 +7,7 @@ char *_memset(char *s, char b, unsigned int n);
 
 char *_memcpy(char *dest, char *src, unsigned int n);
 
-char *_strchr(char *s , char c);
+char *_strchr(char *s, char c);
 
 unsigned int _strspn(char *s, char *accept);
 
@@ -20,5 +20,7 @@ void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 
 void set_string(char **s, char *to);
+
+int _strncmp(const char *s1, const char *s2, int n);
 
 #endif/*HOLBERTON*/
