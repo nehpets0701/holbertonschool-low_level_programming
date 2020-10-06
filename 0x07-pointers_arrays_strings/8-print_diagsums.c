3b5e2b556d69e15a9b63ed7d9ad989b639a98347
nehpets0701 <nehpets0701@gmail.com>
2020-10-06 17:57:05 +0000

    diagonal

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "holberton.h"
 
 /**
@@ -8,5 +9,19 @@
  */
 void print_diagsums(int *a, int size)
 {
+	int i;
+	int sum = 0;
 
+	for (i = 0 ; i < size * size ; i =  i + (size + 1))
+	{
+		sum = sum + a[i];
+	}
+
+	printf("%d, ", sum);
+
+	for (i = size - 1, sum = 0 ; i <= size * (size - 1) ; i = i + (size - 1))
+	{
+		sum = sum + a[i];
+	}
+	printf("%d\n", sum);
 }
