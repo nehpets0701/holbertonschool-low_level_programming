f6b665e0444c3ba82cc8f4595f9c7430fd495157
nehpets0701 <nehpets0701@gmail.com>
2020-10-07 16:26:23 +0000

    puts

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ *_puts_recursion-puts
+ *Return:0
+ *@s:input
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{		
+		_putchar('\n');
+		return;
+	}
+	else
+	{
+		_putchar(*s);
+		s++;
+		_puts_recursion(s);
+	}
+}
