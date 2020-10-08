6f6acf4e70057d30b4b50ac322ef4a6ac8116c8a
nehpets0701 <nehpets0701@gmail.com>
2020-10-08 17:14:53 +0000

    sqrtrec

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,11 +1,28 @@
 #include "holberton.h"
 
 /**
+ *findSquare-findsquares
+ *Return: int
+ *@n:input
+ *@i:input
+ */
+int findSquare(int n, int i)
+{
+	if (i * i == n)
+		return (i);
+
+	if (i * i > n)
+		return (-1);
+	return (findSquare(n, i + 1));
+}
+
+
+/**
  *_sqrt_recursion-square root
  *Return:int
  *@n:input
  */
 int _sqrt_recursion(int n)
 { 
-	return (n * _sqrt_recursion(n, (1 / 2)));
+	return findSquare(n, 1);
 }
