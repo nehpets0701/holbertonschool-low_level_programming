854a558c5e69ec23be7a714a9e2476513e274d31
nehpets0701 <nehpets0701@gmail.com>
2020-10-08 17:37:50 +0000

    primerec

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,24 +1,36 @@
 #include "holberton.h"
 
 /**
+ *findPrime-prime
+ *Return:int
+ *@n:input
+ *@i:input
+ */
+int findPrime(int n, int i)
+{
+	if (i < n / 2)
+	{
+		if (n % i == 0)
+		{
+			return (0);
+		}
+	}
+	if (i >= n / 2)
+		return (1);
+
+	return (findPrime(n, i + 1));
+}
+
+/**
  *is_prime_number-prime
  *Return:0/1
  *@n:input
  */
 int is_prime_number(int n)
 {
-	int i = n / 2;
-
-	if (i == 1)
-		return 1;
-	else
-	{
-		if (n % i == 0)
-			return (0);
-		else
-			i--;
-		is_prime_number(n);
-	}
-
-	return (0);
+	if (n <= 1)
+		return (0);
+	if (n == 2)
+		return (1);
+	return (findPrime(n, 2));
 }
