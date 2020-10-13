807b789d150526f65fdd312d69e086dec98740ef
nehpets <nehpets0701@gmail.com>
2020-10-13 16:31:31 +0000

    array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *create_array-array
+ *Return:char
+ *@size:size
+ *@c:c
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *ptr;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+	else
+	{
+		ptr = malloc(size * sizeof(char));		
+
+		if (ptr == NULL)
+			return (NULL);
+		else
+		{
+			for (i = 0 ; i <= size ; i++)
+			{
+				ptr[i] = c;
+			}
+		}
+	}
+	return (ptr);
+}
