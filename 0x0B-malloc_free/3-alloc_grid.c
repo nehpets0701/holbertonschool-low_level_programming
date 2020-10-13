4ae09b7c2687d71e31efc32a9b53a771b3066d0a
nehpets <nehpets0701@gmail.com>
2020-10-13 20:36:34 +0000

    grid

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *alloc_grid-alloc
+ *Return:int
+ *@width:input
+ *@height:input
+ */
+int **alloc_grid(int width, int height)
+{
+	int **ptr;
+	int i;
+	int j;
+	int b;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	ptr = malloc(sizeof(int *) * height);
+	if (ptr == NULL)
+		return (NULL);
+	for (b = 0 ; b <= width ; b++)
+		ptr[b] = malloc(width * sizeof(int));
+	for (i = 0 ; i <= height ; i++)
+	{
+		for (j = 0 ; j < width ; j++)
+		{
+			ptr[i][j] = 0;
+		}
+	}
+	return (ptr);
+}
