3283ff228bd080cef520a5c2050197eaff207184
nehpets <nehpets0701@gmail.com>
2020-10-16 17:12:22 +0000

    realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -11,42 +11,43 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-        unsigned int count = 0, i = 0;
-        char *trash, *ptr1 = ptr;
-        if (new_size == old_size)
-                return (ptr);
-        if (new_size == 0 && ptr != NULL)
-        {
-                free(ptr);
-                return (NULL);
-        }
-        if (ptr == NULL)
-        {
-                ptr = malloc(new_size);
-                if (ptr == NULL)
-                        return (NULL);
-                return (ptr);
-        }
-        if (new_size < old_size)
-                old_size = new_size;
-        trash = malloc(new_size);
-        if (trash == NULL)
-        {
-                free(trash);
-                return (NULL);
-        }
-        count = (new_size - old_size) + 1;
-        for (i = 0; i < count; i++)
-                trash[i] = ptr1[i];
-        free(ptr);
-        ptr1 = malloc(new_size);
-        if (ptr1 == NULL)
-        {
-                free(ptr1);
-                free(trash);
-		[12:50 PM] return (NULL);
-        }
-        for (i = 0; i < new_size; i++)
-                ptr1[i] = trash[i];
-        return (ptr1);
+	unsigned int count = 0, i = 0;
+	char *trash, *ptr1 = ptr;
+
+	if (new_size == old_size)
+		return (ptr);
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (ptr == NULL)
+	{
+		ptr = malloc(new_size);
+		if (ptr == NULL)
+			return (NULL);
+		return (ptr);
+	}
+	if (new_size < old_size)
+		old_size = new_size;
+	trash = malloc(new_size);
+	if (trash == NULL)
+	{
+		free(trash);
+		return (NULL);
+	}
+	count = (new_size - old_size) + 1;
+	for (i = 0; i < count; i++)
+		trash[i] = ptr1[i];
+	free(ptr);
+	ptr1 = malloc(new_size);
+	if (ptr1 == NULL)
+	{
+		free(ptr1);
+		free(trash);
+		return (NULL);
+	}
+	for (i = 0; i < new_size; i++)
+		ptr1[i] = trash[i];
+	return (ptr1);
 }
