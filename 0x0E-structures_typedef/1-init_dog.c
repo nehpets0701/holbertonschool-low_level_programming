42a178e652379574cb768517407f84fb15f68433
nehpets <nehpets0701@gmail.com>
2020-10-19 18:29:43 +0000

    init

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
-#include "holberton.h"
+#include "dog.h"
+#include <stdlib.h>
 
 /**
  *init-dog-dog
@@ -10,5 +11,7 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
