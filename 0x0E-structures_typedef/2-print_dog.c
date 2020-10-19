7b2000e75132c325c30c35aa76c3e71f7c3c1e99
nehpets <nehpets0701@gmail.com>
2020-10-19 18:42:30 +0000

    print

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,4 +1,6 @@
-#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
 
 /**
  *print_dog-print
@@ -7,5 +9,7 @@
  */
 void print_dog(struct dog *d)
 {
-
+	printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", d->owner);
 }
