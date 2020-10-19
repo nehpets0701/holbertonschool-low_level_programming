87ca02b88846394b5bd9c4e8e969d0c1361c8a27
nehpets <nehpets0701@gmail.com>
2020-10-19 22:54:13 +0000

    new

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,13 +1,53 @@
-#include "holberton.h"
+#include <stdlib.h>
+#include "dog.h"
 
 /**
- *dog_t-dog
+ *new_dog-dog
  *@name:name
  *@age:age
  *@owner:owner
+ *Return:dog_t
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t;
-	return (0);
+	dog_t *dog_dog;
+	char *ownerVar;
+	char *nameVar;
+	int i;
+
+	dog_dog = malloc(sizeof(dog_t));
+	if (dog_dog == NULL)
+	{
+		free(dog_dog);
+		return (NULL);
+	}
+	ownerVar = malloc(sizeof(owner) + 1);
+	if (ownerVar == NULL)
+	{
+		free(dog_dog);
+		free(ownerVar);
+		return (NULL);
+	}
+	for (i = 0; name[i] != '\0'; i++)
+		nameVar[i] = name[i];
+
+	ownerVar[i] = '\0';
+	nameVar = malloc(sizeof(name) + 1);
+	if (nameVar == NULL)
+	{
+		free(dog_dog);
+		free(ownerVar);
+		free(nameVar);
+		return (NULL);
+	}
+
+	for (i = 0; name[i] != '\0'; i++)
+		nameVar[i] = name[i];
+
+	nameVar[i] = '\0';
+	dog_dog->name = nameVar;
+	dog_dog->age = age;
+	dog_dog->owner = ownerVar;
+
+	return (dog_dog);
 }
