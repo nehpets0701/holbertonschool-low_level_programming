6ed15660545f4205e9e570be3044204adf837e88
nehpets <nehpets0701@gmail.com>
2020-10-20 00:02:12 +0000

    free

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,4 +1,5 @@
-#include "holberton.h"
+#include "dog.h"
+#include <stdlib.h>
 
 /**
  *free_dog-free
@@ -7,6 +8,10 @@
  */
 void free_dog(dog_t *d)
 {
-	free(dog_t);
-	return (0);
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
 }
