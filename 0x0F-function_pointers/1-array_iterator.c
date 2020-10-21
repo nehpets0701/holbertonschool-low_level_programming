a2cb9a13e8428863a25358095f321175efb4e35a
nehpets <nehpets0701@gmail.com>
2020-10-21 17:13:49 +0000

    iterate

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -7,7 +7,15 @@
  *@size:size
  *@action:pointer
  */
-void array_iterator(int *array, size_t size, void (*action)(int));
+void array_iterator(int *array, size_t size, void (*action)(int))
 {
+	unsigned int i;
 
+	if (array == NULL)
+		return;
+	if (action != NULL)
+	{
+		for (i = 0; i < size; i++)
+			action(i);
+	}
 }
