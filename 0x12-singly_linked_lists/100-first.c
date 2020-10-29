a66b8a1ae8a95d3dccc0138bc4e0594f99c87927
nehpets <nehpets0701@gmail.com>
2020-10-29 19:15:12 +0000

    pushing advanced

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ *preMain-premain
+ *Return:0
+ */
+int preMain(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	return (0);
+}
