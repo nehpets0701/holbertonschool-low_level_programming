9f9136338e4df47304c4211092616974745d3bff
nehpets <nehpets0701@gmail.com>
2020-10-29 16:48:35 +0000

    fixing len check

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,5 +1,7 @@
 #include "lists.h"
 #include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
  *print_list-prints list
@@ -8,5 +10,17 @@
  */
 size_t print_list(const list_t *h)
 {
+	int count = 0;
+	const list_t *current = h;
 
+	while (current != NULL)
+	{
+		if (current->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", current->len, current->str);
+		current = current->next;
+		count++;
+	}
+	return (count);
 }
