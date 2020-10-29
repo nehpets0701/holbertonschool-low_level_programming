0fb1338ebe8cad4c2dc4e77e08e294dbac17faab
nehpets <nehpets0701@gmail.com>
2020-10-29 17:52:53 +0000

    pushing 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,5 +1,7 @@
 #include "lists.h"
+#include<string.h>
 #include <stddef.h>
+#include <stdlib.h>
 
 /**
  *add_node-add node
@@ -9,5 +11,21 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
+	list_t *new;
+	int length = 0;
 
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+
+	new->str = strdup(str);
+	new->len = length;
+	new->next = *head;
+	*head = new;
+	return (*head);
 }
