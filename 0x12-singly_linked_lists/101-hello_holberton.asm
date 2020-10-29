16938fae63fe474fdba56fd3b53177fe32c99021
nehpets <nehpets0701@gmail.com>
2020-10-29 19:25:25 +0000

    assembly?

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,26 +1,27 @@
-	section .text		;section declaration
+.text                           # section declaration
 
-	;; we must export the entry point to the ELF linker or
-	    global  _start	;loader. They conventionally recognize _start as their
-	;; entry point. Use ld -e foo to override the default.
+			                          # we must export the entry point to the ELF linker or
+	    .global _start              # loader. They conventionally recognize _start as their
+			                          # entry point. Use ld -e foo to override the default.
 
 _start:
 
-	;; write our string to stdout
+	                                # write our string to stdout
 
-	    mov     edx,len	;third argument: message length
-	    mov     ecx,msg	;second argument: pointer to message to write
-	    mov     ebx,1	;first argument: file handle (stdout)
-	    mov     eax,4	;system call number (sys_write)
-	    int     0x80	;call kernel
+	  movl    $len,%edx           # third argument: message length
+	  movl    $msg,%ecx           # second argument: pointer to message to write
+	  movl    $1,%ebx             # first argument: file handle (stdout)
+	  movl    $4,%eax             # system call number (sys_write)
+	  int     $0x80               # call kernel
 
-	;; and exit
+	                                # and exit
 
-	mov     ebx,0		;first syscall argument: exit code
-	    mov     eax,1	;system call number (sys_exit)
-	    int     0x80	;call kernel
+	  movl    $0,%ebx             # first argument: exit code
+	  movl    $1,%eax             # system call number (sys_exit)
+	  int     $0x80               # call kernel
 
-	section .data		;section declaration
+	.data                           # section declaration
 
-	msg db      "Hello, Holberton",0xa ;our dear string
-	len equ     $ - msg             ;length of our dear string
+msg:
+	.ascii    "Hello, Holberton\n"   # our dear string
+	len = . - msg                 # length of our dear string
