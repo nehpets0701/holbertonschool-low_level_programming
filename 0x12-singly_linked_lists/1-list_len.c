bdd6f5747368f4f5f6cc169dc0b957b76abf87d6
nehpets <nehpets0701@gmail.com>
2020-10-29 16:53:49 +0000

    pushing 1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -7,5 +7,16 @@
  */
 size_t list_len(const list_t *h)
 {
+	const list_t *current = h;
+	int length = 1;
 
+	if (current == NULL)
+		return (0);
+
+	while (current->next != NULL)
+	{
+		current = current->next;
+		length++;
+	}
+	return (length);
 }
