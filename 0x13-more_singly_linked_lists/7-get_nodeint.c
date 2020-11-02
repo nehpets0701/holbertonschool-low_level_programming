0c4fbdfa1a12b2e75ceb353f3f8d4ae3635a0d8b
nehpets <nehpets0701@gmail.com>
2020-11-02 19:38:02 +0000

    get

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,5 +11,17 @@
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
+	unsigned int count = 0;
+	
+	if (head == NULL)
+		return (NULL);
 
+	while (head != NULL)
+	{
+		if (count == index)
+			return (head);
+		count++;
+		head = head->next;
+	}
+	return (NULL);
 }
