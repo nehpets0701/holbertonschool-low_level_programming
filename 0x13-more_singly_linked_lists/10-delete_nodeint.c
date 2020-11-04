61e2fc03246b5c4a0c19702a23e1084b51ab69df
nehpets <nehpets0701@gmail.com>
2020-11-04 02:41:44 +0000

    pushing 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -4,12 +4,32 @@
 #include <stddef.h>
 
 /**
- *delete_nodeint_at_index
+ *delete_nodeint_at_index-delete n
  *Return: 1 if success, -1 if failure
  *@head:start of list
  *@index:index to delete
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
+	listint_t *current = *head;
+	listint_t *temp;
+	unsigned int i = 0;
 
+	if (*head == NULL)
+		return (-1);
+
+	if (index == 0)
+	{
+		*head = current->next;
+		free(current);
+		return (1);
+	}
+
+	for (i = 0; i < index - 1; i++)
+		current = current->next;
+
+	temp = current->next->next;
+	free(current->next);
+	current->next = temp;
+	return (1);
 }
