adc035fe87be4efe05cf6ace21fb90ab6b7a36a4
nehpets <nehpets0701@gmail.com>
2020-11-04 18:24:43 +0000

    adding length check

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -4,6 +4,28 @@
 #include <stddef.h>
 
 /**
+ *max-max
+ *Return: number of nodes
+ *@h: head
+ */
+size_t max(const listint_t *h)
+{
+	const listint_t *temp = h;
+	int maximum = 0;
+
+	if (temp == NULL)
+		return (0);
+
+	while (temp != NULL)
+	{
+		temp = temp->next;
+		maximum++;
+	}
+
+	return (maximum - 1);
+}
+
+/**
  *insert_nodeint_at_index-insert
  *Return: adress of new node or null if fails
  *@head: start of list
@@ -13,7 +35,7 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *temp, *new, *headTemp;
-	unsigned int count = 0;
+	unsigned int count = 0, maximum = max(*head);
 
 	if (head == NULL)
 		return (NULL);
@@ -22,6 +44,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
+
+	if (idx > maximum)
+		return (-1);
 
 	headTemp = *head;
 	while (headTemp != NULL)
