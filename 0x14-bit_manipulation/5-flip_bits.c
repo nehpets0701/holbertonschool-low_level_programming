e195415191120f4827f1bae8c1e6299abcad26ca
nehpets <nehpets0701@gmail.com>
2020-11-05 19:53:04 +0000

    trying to fix last check

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -11,12 +11,16 @@
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	int count = 0;
-	int a = n ^ m;
 
-	while (a > 0 && n > 0 && m > 0)
+	while (n > 0 || m > 0)
 	{
-		count++;
-		a &= (a - 1);
+		if ((n & 1) != (m & 1))
+		{
+			count++;
+		}
+
+		n = n >> 1;
+		m = m >> 1;
 	}
 	return (count);
 }
