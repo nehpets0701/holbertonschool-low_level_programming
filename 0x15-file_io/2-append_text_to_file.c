f5bb4487041b0604e7c29025b6c019d380d7be7a
nehpets <nehpets0701@gmail.com>
2020-11-09 21:32:03 +0000

    betty

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -27,7 +27,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	{
 		while (text_content[length] != '\0')
-		       length++;
+			length++;
 
 		charsWritten = write(file, text_content, length);
 		if (charsWritten == -1)
