55d83e5efa635c97ceeab1751efd01ab82abfa2a
nehpets <nehpets0701@gmail.com>
2020-11-09 19:53:52 +0000

    pushing 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -4,7 +4,6 @@
 #include <stdlib.h>
 #include <stddef.h>
 
-
 /**
  *create_file-makes a file
  *Return:1/-1
@@ -13,5 +12,24 @@
  */
 int create_file(const char *filename, char *text_content)
 {
+	int file, charsWritten, length = 0;
 
+	if (filename == NULL)
+		return (-1);
+
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (file == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		while (text_content[length] != '\0')
+			length++;
+
+		charsWritten = write(file, text_content, len);
+		if (charsWritten == -1)
+			return (-1);
+	}
+	close(file);
+	return (1);
 }
