0ff4d031b7f946e080fd979c4bedd97556022d9a
nehpets <nehpets0701@gmail.com>
2020-11-09 18:24:38 +0000

    pushing 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,6 +3,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include "holberton.h"
 
 /**
  *read_textfile-read
@@ -12,5 +15,38 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	
+	int file, charsPrinted = 0, charsRead = 0;
+	char *str;
+
+	file = open(filename, O_RDONLY);
+	if (file == -1)
+		return (0);
+
+	str = malloc(sizeof(char) * letters);
+	if (str == NULL)
+	{
+		close(file);
+		return (0);
+	}
+
+	charsRead = read(file, str, letters);
+	if (charsRead == -1)
+	{
+		close(file);
+		free(str);
+		return (0);
+	}
+
+	str[charsRead] = '\0';
+	charsPrinted = write(STDOUT_FILENO, str, charsRead);
+	if (charsPrinted == -1 || charsPrinted != charsRead)
+	{
+		close(file);
+		free(str);
+		return (0);
+	}
+
+	close(file);
+	free(str);
+	return (charsPrinted);
 }
