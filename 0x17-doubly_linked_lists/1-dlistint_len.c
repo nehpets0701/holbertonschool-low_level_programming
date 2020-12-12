febe633209f479a350cb063268a88a5648a2e5ba
nehpets <nehpets0701@gmail.com>
2020-12-12 01:47:29 +0000

    betty

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,11 +1,16 @@
 #include "lists.h"
 
+/**
+ * dlistint_len-length
+ * Return:count
+ * @h:input
+ */
 size_t dlistint_len(const dlistint_t *h)
 {
-    int count = 0;
+	int count = 0;
 
-    while (h != NULL)
-    {
+	while (h != NULL)
+	{
 		count++;
 		h = h->next;
 	}
