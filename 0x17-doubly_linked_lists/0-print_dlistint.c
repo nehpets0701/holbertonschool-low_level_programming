bc06d17316d3f4b2cf52d09d5ef821918898e5c1
nehpets <nehpets0701@gmail.com>
2020-12-12 01:44:57 +0000

    betty

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,10 +1,15 @@
 #include "lists.h"
 
+/**
+ *print_dlistint-print
+ * Return:count
+ * @h:input
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
-    int count = 0;
-	
-    while (h != NULL)
+	int count = 0;
+
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		count++;
