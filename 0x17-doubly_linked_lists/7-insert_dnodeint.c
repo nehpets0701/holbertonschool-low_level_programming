6423a66e17573fa632f82bd6ebdfbafca1ca3dc8
nehpets <nehpets0701@gmail.com>
2020-12-13 07:32:02 +0000

    pushing 7

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index-insert
+ * Return:list
+ * @h:input
+ * @idx:where to put it
+ * @n:value
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+
+}
