9563c180ba3b6cf3a0663d55c6fd4cbbcabd9d8f
nehpets <nehpets0701@gmail.com>
2020-12-13 07:35:48 +0000

    idx > i

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -32,5 +32,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		i++;
 		current = current->next;
 	}
+
+	if (idx > i)
+		return (NULL);
+
 	return (add_dnodeint_end(&*h, n));
 }
