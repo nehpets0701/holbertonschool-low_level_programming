48dc15fc72bc40bb239c83b616e30c79c5b2b78e
nehpets <nehpets0701@gmail.com>
2020-12-13 05:40:49 +0000

    null check

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -15,8 +15,12 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 
 	new->n = n;
-	new->prev = NULL;
 	new->next = *head;
+	new->prev = NULL;
+
+	if (*head != NULL)
+		(*head)->prev = new;
+
 	*head = new;
-	return (*head);
+	return (new);
 }
