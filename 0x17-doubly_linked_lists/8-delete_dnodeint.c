517dbf50627e1dd5224837ed2b76f5d4bafd3dd0
nehpets <nehpets0701@gmail.com>
2020-12-14 00:35:03 +0000

    pushing 8

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index-delete
+ * Return:list
+ * @head:input
+ * @index:which one to delete
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+
+}
