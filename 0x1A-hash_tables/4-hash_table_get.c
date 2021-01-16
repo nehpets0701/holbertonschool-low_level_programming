9c01fc5bce4f78f3356a909ba3858ca71be9ffb6
nehpets <nehpets0701@gmail.com>
2021-01-16 01:14:09 +0000

    pushing 4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -8,5 +8,17 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
+	hash_node_t *node;
 
+	if (key == NULL || !(*key) || ht == NULL)
+		return (NULL);
+
+	node = ht->array[hash_djb2((unsigned char *)key) % ht->size];
+	while (node != NULL)
+	{
+		if (strcmp(node->key, (char *)key) == 0)
+			return (node->value);
+		node = node->next;
+	}
+	return (NULL);
 }
