2efc7835f440d2472f2bc0e5f0e1ddae316a6b3f
nehpets <nehpets0701@gmail.com>
2021-01-16 02:13:19 +0000

    pushing\5

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -6,5 +6,22 @@
  */
 void hash_table_print(const hash_table_t *ht)
 {
+	unsigned long int i, count;
+	hash_node_t *node;
 
+	if (ht == NULL)
+		return;
+	printf("{");
+	count = 0;
+	for (i = 0; i < ht->size; i++)
+	{
+		for (node = ht->array[i]; node != NULL; node = node->next)
+		{
+			if (count > 0)
+				printf(", ");
+			printf("'%s': '%s'", node->key, node->value);
+			count += 1;
+		}
+	}
+	printf("}\n");
 }
