3e1da3b584df1f0a0bf5ec1e636f6a8cbf3b3916
nehpets <nehpets0701@gmail.com>
2021-01-16 02:19:09 +0000

    pushing 6

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -6,5 +6,25 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
+	unsigned long int i = 0;
+	hash_node_t *temp;
 
+	if (ht == NULL)
+		return;
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i])
+		{
+			while (ht->array[i] != NULL)
+			{
+				temp = ht->array[i];
+				ht->array[i] = ht->array[i]->next;
+				free(temp->key);
+				free(temp->value);
+				free(temp);
+			}
+		}
+	}
+	free(ht->array);
+	free(ht);
 }
