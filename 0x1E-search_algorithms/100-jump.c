d85936e1c11916269589977c0edd47d9d57fcee8
Stephen <nehpets0701@gmail.com>
2021-04-21 21:33:00 -0400

    doesnt work

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,40 @@
+#include "search_algos.h"
+
+/**
+*jump_search-jump search
+*Return:index of value
+*@array:input
+*@size:size
+*@value:value to look for
+*/
+int jump_search(int *array, size_t size, int value)
+{
+	unsigned int step = size ^ (1 / 2);
+	unsigned int i, j = 0;
+
+	if (array == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i = i + step)
+	{
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+
+		if (array[i] >= value)
+		{
+			if (array[i] == value)
+				return (value);
+			else
+			{
+				for (j = i - step; j == i; j++)
+				{
+					if (array[j] == value)
+						return (array[j]);
+				}
+
+				return (-1);
+			}
+		}
+	}
+
+	return (-1);
+}
